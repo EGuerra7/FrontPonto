a81ee1958a3a8cc6189213ab86e6eceec9d278f7
EGuerra7 <erickmuzzi@gmail.com>
2024-08-28 12:15:06 -0300

    layout para o header

--- src/app/components/cadastro/cadastro.component.ts
+++ src/app/components/cadastro/cadastro.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from "../header/header.component";
 
 @Component({
   selector: 'app-cadastro',
   standalone: true,
-  imports: [],
+  imports: [HeaderComponent],
   templateUrl: './cadastro.component.html',
   styleUrl: './cadastro.component.css'
 })
