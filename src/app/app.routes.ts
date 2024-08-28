d7de6cc01c64d7d70494b9999ee84d58350cd12a
EGuerra7 <erickmuzzi@gmail.com>
2024-08-28 17:37:08 -0300

    Home e login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,7 +6,7 @@ import { LoginComponent } from './components/login/login.component';
 export const routes: Routes = [
   {
     path: '',
-    redirectTo: "/home",
+    redirectTo: "/login",
     pathMatch: 'full'
   },
   {
--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from '../shared/header/header.component';
+import { BlueboxComponent } from "../shared/bluebox/bluebox.component";
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [HeaderComponent, BlueboxComponent],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
